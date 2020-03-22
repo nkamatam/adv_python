6e99e7c5eef316707e69656382cc906b888a3cbc
kishan-mashru <kishan.p.mashru@gmail.com>
2020-03-22 22:29:59 +0000

    logger changes

---
+++ python_log/.ipynb_checkpoints/power-checkpoint.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(filename = 'pow.log', level=logging.INFO, format='%(levelname)s:%(asctime)s:%(message)s')
+
+def squ(num):
+    return num * num
+
+a=10
+result_squ = squ(a)
+logging.info('Square : {}  = {}'.format(a,result_squ))
